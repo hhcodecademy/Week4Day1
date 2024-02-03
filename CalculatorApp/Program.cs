c902c5c7b7e6c8d45e3b57c4cebb904bb9c499cf
Huseyn Hasanli <huseyn.hasanli@code.edu.az>
2024-02-03 19:58:51 +0400

    Add project files.

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace CalculatorApp
+{
+    internal class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.ededi daxil ediniz");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("2.ededi daxil ediniz");
+            int number2 = int.Parse(Console.ReadLine());
+
+            int addResult =  add(number1,number2,5);     
+
+            Console.WriteLine($"Tolamananin neticesi {addResult}");
+
+            int subtractionResult = substraction(number1,number2);
+
+            Console.WriteLine($"Cixmanim neticesi {subtractionResult}");
+
+            int multiplyResult = multiply(number1,number2);
+            Console.WriteLine($"Vurmanin neticesi {multiplyResult}");
+
+            float divideResult =divide(number1,number2);
+            Console.WriteLine($"Bolmenin neticesi {divideResult}");
+
+
+        }
+
+
+
+
+        public static int add(int num1, int num2)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+      
+        public static int add(int num1, float num2)
+        {
+            int result = num1 + (int)num2;
+            return result;
+        }
+        public static int add(int num1, int num2,int num3) {
+            int result =num1 + num2+num3;
+            return result;
+        }
+
+        public static int substraction(int num1, int num2) { 
+        
+            int result = num1 - num2;
+            return result;
+        
+        }
+
+        public static int multiply(int num1, int num2)
+        {
+            int result=num1 * num2;
+            return result;
+            
+        }
+
+        public static float divide(float num1, float num2) {
+        
+             float result = num1 / num2;
+             return result;
+        }
+    }
+}
---
+++ SMSApp/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace SMSApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Telebenin nomresini daxil ediniz");
+            string studentNumber = Console.ReadLine();
+            string fName="Nihad";
+            string uName = ""; ;
+            //float resultGrade = GetStudentInfo(studentNumber, out fName, out uName);
+
+            float resultGrade2 = GetStudentInfo2(studentNumber, ref fName, ref uName);
+        }
+        public static float GetStudentInfo(string studentNumber, out string fullName, out string universityName)
+        {
+
+            float grade = 0;
+            fullName = "";
+            universityName = "";
+
+            if (studentNumber == "1252")
+            {
+                fullName = "Huseyn Hesenli";
+                universityName = "Ege unv";
+                grade = 80;
+            }
+
+            else if (studentNumber == "1253")
+            {
+                fullName = "Semra Alb";
+                universityName = "Iktisad unv";
+                grade = 85;
+            }
+            else if (studentNumber == "1254")
+            {
+                fullName = "Nezrin Hesenova";
+                universityName = "Azerbaycan Tec unv";
+                grade = 78;
+            }
+            else if (studentNumber == "1255")
+            {
+                fullName = "Ugur Qasimoc";
+                universityName = "Neft ve Senaye unv";
+                grade = 100;
+            }
+            return grade;
+        }
+        public static float GetStudentInfo2(string studentNumber, ref string fullName, ref string universityName)
+        {
+
+            float grade = 0;
+
+            if (studentNumber == "1252")
+            {
+                fullName = "Huseyn Hesenli";
+                universityName = "Ege unv";
+                grade = 80;
+            }
+
+            else if (studentNumber == "1253")
+            {
+                fullName = "Semra Alb";
+                universityName = "Iktisad unv";
+                grade = 85;
+            }
+            else if (studentNumber == "1254")
+            {
+                fullName = "Nezrin Hesenova";
+                universityName = "Azerbaycan Tec unv";
+                grade = 78;
+            }
+            else if (studentNumber == "1255")
+            {
+                fullName = "Ugur Qasimoc";
+                universityName = "Neft ve Senaye unv";
+                grade = 100;
+            }
+            return grade;
+        }
+    }
+}
